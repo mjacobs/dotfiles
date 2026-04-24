6fa025d83e08411251946683ac8a3c93258e1bb4
Matthew Jacobs <mjacobs@apache.org>
2026-04-24 12:24:38 -0700

    minor tweaks; claude & nvim config drift

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -30,7 +30,6 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  dev = { path = "~/dd/gh" },
   install = { colorscheme = { "backpack", "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
